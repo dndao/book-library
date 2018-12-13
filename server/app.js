f5671165b2d9a04b1ed9a5551d7dda574a08cc9a
Daniel Dao <guangdanny@gmail.com>
2018-12-13 22:19:48 +1100

    mongoose setup, mongodb server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
+
+mongoose.connect('mongodb://'+config.MONGODB_USERNAME+':'+config.MONGODB_PASSWORD+'@ds133290.mlab.com:33290/gql-dd');
+mongoose.connection.once('open', ()=> {
+  console.log('connected to database');
+})
 
 app.use('/graphql',graphqlHTTP({
   schema,
