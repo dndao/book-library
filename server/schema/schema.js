3caa77acf444c7f66dc8ce3c8767ced93f46950d
Daniel Dao <guangdanny@gmail.com>
2018-12-13 22:30:45 +1100

    MongoDB models

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 const {GraphQLObjectType,
   GraphQLString,
@@ -8,22 +10,6 @@ const {GraphQLObjectType,
   GraphQLInt,
   GraphQLList
 } = graphql;
-
-
-var books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
-  {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
-  {name: 'The Color of Magic', genre: 'Sci-Fi', id: '5', authorId: '3'},
-  {name: 'The Light Fantastic', genre: 'Sci-Fi', id: '6', authorId: '3'}
-]
-
-var authors = [
-  {name: 'Patrick Rothfuss', age: 44, id: '1'},
-  {name: 'Brandon Sanderson', age: 42, id: '2'},
-  {name: 'Terry Pratchett', age: 66, id: '3'}
-]
 
 const BookType = new GraphQLObjectType({
   name:'Book',
