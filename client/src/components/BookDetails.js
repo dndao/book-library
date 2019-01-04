edb95242a02601d461e8f1f33428edd477de3da4
Daniel Dao <guangdanny@gmail.com>
2019-01-04 22:48:56 +1100

    change book detail format

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -9,9 +9,9 @@ class BookDetails extends Component {
       return(
         <div>
         <h2>{ book.name }</h2>
-        <p>{ book.genre }</p>
-        <p>{ book.author.name }</p>
-        <p>All books by this author:</p>
+        <p>Genre: { book.genre }</p>
+        <p>Author: { book.author.name }</p>
+        <p>Other books by { book.author.name }:</p>
         <ul className="other-books">
         { book.author.books.map(item => {
           return <li key={item.id}>{ item.name }</li>
